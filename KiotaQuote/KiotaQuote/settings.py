3f4a5ebdf689525452d79efe5142633ddfd4d792
dmatano <duenkip@gmail.com>
2021-12-12 11:32:48 +0300

    fix

--- KiotaQuote/KiotaQuote/settings.py
+++ KiotaQuote/KiotaQuote/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*#-q!b+vxu-)ncu-p8!i0o756j@!c1_c=ee+&@%745$ir@jnps'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
